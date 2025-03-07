4f2ceb9f75f8e8668e2e652ac59b2f3410eb69df
Jay Shaffer <jay.shaffer@pathccm.com>
2025-03-07 16:54:21 -0700

    Some things

--- main.py
+++ main.py
@@ -8,16 +8,24 @@ from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.app import App
 from kivy.uix.widget import Widget
-
+from kivy.graphics import Color, Rectangle
+from kivy.clock import Clock
+import copy
+import random
+import json
+import asyncio
+import pyz3r
+import os
 
 glitches_dropdown = DropDown()
 glitches_options = [
     ["none", "No Glitches"],
-    ["overworld glitches", "Overworld Glitches"],
-    ["major glitches", "Major Glitches"],
-    ["no logic", "No Logic"],
+    ["overworld_glitches", "Overworld Glitches"],
+    ["hybrid_major_glitches", "Hybrid Major Glitches"],
+    ["major_glitches", "Major Glitches"],
+    ["no_logic", "No Logic"],
 ]
-glitches_container = ["Glitches", glitches_dropdown, glitches_options]
+glitches_container = ["glitches", "Glitches", glitches_dropdown, glitches_options]
 
 item_placement_dropdown = DropDown()
 item_placement_options = [
@@ -25,6 +33,7 @@ item_placement_options = [
     ["advanced", "Advanced"],
 ]
 item_placement_container = [
+    "item_placement",
     "Item Placement",
     item_placement_dropdown,
     item_placement_options,
@@ -33,19 +42,20 @@ item_placement_container = [
 shuffle_dropdown = DropDown()
 shuffle_options = [
     ["standard", "Standard"],
-    ["mapscompasses", "Maps / Compasses"],
-    ["mapscompassessmallkeys", "Maps / Compasses / Small Keys"],
-    ["keysanity", "Keysanity"],
+    ["mc", "Maps / Compasses"],
+    ["mcs", "Maps / Compasses / Small Keys"],
+    ["full", "Keysanity"],
 ]
-shuffle_container = ["Shuffle", shuffle_dropdown, shuffle_options]
+shuffle_container = ["shuffle", "Shuffle", shuffle_dropdown, shuffle_options]
 
 accessibility_dropdown = DropDown()
 accessibility_options = [
-    ["100percentinventory", "100% Inventory"],
-    ["100percentlocations", "100% Locations"],
-    ["beatableonly", "Beatable Only"],
+    ["inventory", "100% Inventory"],
+    ["locations", "100% Locations"],
+    ["none", "Beatable Only"],
 ]
 accessibility_container = [
+    "accessibility",
     "Accessibility",
     accessibility_dropdown,
     accessibility_options,
@@ -54,14 +64,14 @@ accessibility_container = [
 goal_dropdown = DropDown()
 goal_options = [
     ["ganon", "Defeat Ganon"],
-    ["fastganon", "Fast Ganon"],
+    ["fast_ganon", "Fast Ganon"],
     ["dungeons", "All Dungeons"],
     ["pedestal", "Master Sword Pedestal"],
-    ["triforcepieces", "Triforce Pieces"],
+    ["triforce-hunt", "Triforce Pieces"],
     ["ganonhunt", "Ganon Hunt"],
     ["completionist", "Completionist"],
 ]
-goal_container = ["Goal", goal_dropdown, goal_options]
+goal_container = ["goal", "Goal", goal_dropdown, goal_options]
 
 open_tower_dropdown = DropDown()
 open_tower_options = [
@@ -75,7 +85,7 @@ open_tower_options = [
     ["0", "0 Crystals"],
     ["random", "Random"],
 ]
-open_tower_container = ["Open Tower", open_tower_dropdown, open_tower_options]
+open_tower_container = ["open_tower", "Open Tower", open_tower_dropdown, open_tower_options]
 
 ganon_vulnerable_dropdown = DropDown()
 ganon_vulnerable_options = [
@@ -90,6 +100,7 @@ ganon_vulnerable_options = [
     ["random", "Random"],
 ]
 ganon_vulnerable_container = [
+    "ganon_vulnerable",
     "Ganon Vulnerable",
     ganon_vulnerable_dropdown,
     ganon_vulnerable_options,
@@ -102,7 +113,7 @@ world_state_options = [
     ["inverted", "Inverted"],
     ["retro", "Retro"],
 ]
-world_state_container = ["World State", world_state_dropdown, world_state_options]
+world_state_container = ["world_state", "World State", world_state_dropdown, world_state_options]
 
 entrance_shuffle_dropdown = DropDown()
 entrance_shuffle_options = [
@@ -113,7 +124,9 @@ entrance_shuffle_options = [
     ["crossed", "Crossed"],
     ["insanity", "Insanity"],
 ]
+
 entrance_shuffle_container = [
+    "entrances",
     "Entrance Shuffle",
     entrance_shuffle_dropdown,
     entrance_shuffle_options,
@@ -126,7 +139,7 @@ boss_shuffle_options = [
     ["full", "Full"],
     ["random", "Random"],
 ]
-boss_shuffle_container = ["Boss Shuffle", boss_shuffle_dropdown, boss_shuffle_options]
+boss_shuffle_container = ["boss_shuffle", "Boss Shuffle", boss_shuffle_dropdown, boss_shuffle_options]
 
 enemy_shuffle_dropdown = DropDown()
 enemy_shuffle_options = [
@@ -151,16 +164,16 @@ swords_options = [
     ["vanilla", "Vanilla"],
     ["swordless", "Swordless"],
 ]
-swords_container = ["Swords", swords_dropdown, swords_options]
+swords_container = ["enemy_shuffle", "Swords", swords_dropdown, swords_options]
 
 item_pool_dropdown = DropDown()
 item_pool_options = [
     ["normal", "Normal"],
     ["hard", "Hard"],
     ["expert", "Expert"],
-    ["crowdcontrol", "Crowd Control"],
+    ["crowd_control", "Crowd Control"],
 ]
-item_pool_container = ["Item Pool", item_pool_dropdown, item_pool_options]
+item_pool_container = ["item_pool", "Item Pool", item_pool_dropdown, item_pool_options]
 
 item_function_dropdown = DropDown()
 item_function_options = [["normal", "Normal"], ["hard", "Hard"], ["expert", "Expert"]]
@@ -176,7 +189,7 @@ enemy_damage_options = [
     ["shuffled", "Shuffled"],
     ["random", "Random"],
 ]
-enemy_damage_container = ["Enemy Damage", enemy_damage_dropdown, enemy_damage_options]
+enemy_damage_container = ["enemy_damage", "Enemy Damage", enemy_damage_dropdown, enemy_damage_options]
 
 enemy_health_dropdown = DropDown()
 enemy_health_options = [
@@ -185,7 +198,7 @@ enemy_health_options = [
     ["hard", "Hard"],
     ["expert", "Expert"],
 ]
-enemy_health_container = ["Enemy Health", enemy_health_dropdown, enemy_health_options]
+enemy_health_container = ["enemy_health", "Enemy Health", enemy_health_dropdown, enemy_health_options]
 
 all_containers = [
     glitches_container,
@@ -206,39 +219,194 @@ all_containers = [
     enemy_damage_container,
     enemy_health_container,
 ]
+
+settings={
+    "glitches": "none",
+    "item_placement": "advanced",
+    "shuffle": "standard",
+    "accessibility": "items",
+    "goal": "ganon",
+    "open_tower": "7",
+    "ganon_vulnerable": "7",
+    "world_state": "open",
+    "entrances": "none",
+    "boss_shuffle": "none",
+    "enemy_shuffle": "none",
+    "swords": "randomized",
+    "item_pool": "normal",
+    "item_function": "normal",
+    "spoilers": "on",
+    "enemy_damage": "default",
+    "enemy_health": "default",
+    "hints": "on",
+}
+
+sprites = [] 
+with open('sprites.json', 'r') as file:
+    data = json.load(file)
+    sprites = data
+
+class GenerateSeedButton(Button):
+    def on_release(self):
+        asyncio.create_task(self.generate_seed())
+        
+    async def generate_seed(self):
+        seed = await pyz3r.ALTTPR.generate(
+            settings={
+                "glitches": settings["glitches"],
+                "item_placement": settings["item_placement"],
+                "dungeon_items": settings["shuffle"],
+                "accessibility": settings["accessibility"],
+                "goal": settings["goal"],
+                "crystals": {
+                    "ganon": settings["ganon_vulnerable"],
+                    "tower": settings["open_tower"], 
+                },
+                "mode": settings["world_state"],
+                "entrances": settings["entrances"],
+                "hints": settings["hints"],
+                "weapons": settings["swords"],
+                "item": {
+                    "pool": settings["item_pool"],
+                    "functionality": settings["item_function"] 
+                },
+                "tournament": False,
+                "spoilers": "on",
+                "lang":"en",
+                "enemizer": {
+                    "boss_shuffle": settings["boss_shuffle"],
+                    "enemy_shuffle": settings["enemy_shuffle"],
+                    "enemy_damage": settings["enemy_damage"],
+                    "enemy_health": settings["enemy_health"]
+                }
+            }
+        )
+        print(seed)
+        print(settings)
+        await seed.create_patched_game(
+            input_filename="dontsueme.sfc", # this should be an ALTTP Japan 1.0 ROM
+            output_filename="output/output.sfc", # this is the output ROM
+            heartspeed='off',
+            heartcolor='red',
+            spritename=sprites[random.randint(0, len(sprites) - 1)]['name'], #can be any sprite listed at https://alttpr.com/sprites
+            music=False, # true or false, defaults true
+            quickswap=True,
+            menu_speed='normal',
+            msu1_resume=True # true or false, defaults true
+        )
+        print(seed.url)
+
+class BorderedButton(Button):
+    def __init__(self, *args, **kwargs):
+        super(BorderedButton, self).__init__(*args, **kwargs)
+        #self.background_color = (0, 0, 0, 0)
+        self.color = (0, 0, 0, 1)
+        self.border = (1, 1, 1, 1)
+        self.border_radius = 10
+        # with self.canvas.before:
+        #     Color(1, 1, 1, 1)
+        #     self.rect = Rectangle(size=self.size, pos=self.pos)
+
 class Dropdowns(BoxLayout):
-    def __init__(self, name, options=[], *args, **kwargs):
+    def __init__(self, key, name, options, defaultOption, *args, **kwargs):
         super(Dropdowns, self).__init__(*args, **kwargs)
+        self.size = Window.size
+        # with self.canvas.before:
+        #     Color(0, 1, 0, 1)  # green; colors range from 0-1 instead of 0-255
+        #     self.rect = Rectangle(size=self.size, pos=self.pos)
+        self.orientation = 'vertical'
+        self.spacing = 10
+        self.padding = [100, 5]
+        self.height = 100
+        self.color = (2, 0, 0, 1)
+        self.key = key
         dropdown = DropDown()
-        self.add_widget(dropdown)
-        button = Button(text=name, size_hint=(None,None), on_release= lambda instance, x: dropdown.open())
+        label = Label(
+            text=name + ":",
+            size_hint=(0.3, 1),
+            text_size=(None, None),
+            halign='right',
+            valign='middle',
+            color=(0.2, 0.2, 0.2, 1),  # Dark gray text
+            bold=True
+        )
+        self.add_widget(label)
+        button = Button(
+            text=name,
+            height=1000,
+            size_hint=(0.8, 1),  # Take 40% of width, full height
+            background_color=(0.3, 0.5, 0.8, 1),
+            color=(1, 1, 1, 1),  # White text
+            on_release=lambda button: dropdown.open(button),
+        )
+        #button.bind(on_release=self.handle_dropdown_open)
+        dropdown.bind(on_select=lambda instance, text: self.handle_dropdown_click(button, text))
         self.add_widget(button)
         for option in options:
+            print(option)
+            option_text = copy.copy(option[1])
+            option_value = copy.copy(option[0])
             option_button = Button(
-                    text=option[1],
-                    height=35,
                     size_hint_y=None,
-                    on_release=lambda x: dropdown.select(option[1]),
+                    height=70,
+                    background_color=(0.95, 0.95, 0.95, 1),  # Almost white
+                    background_normal='',  # Remove the default background
+                    color=(0.2, 0.2, 0.2, 1),  # Dark gray text
+                    border=(1, 1, 1, 1),
+                    text=option_text,
+                    on_release=lambda btn, text=option_text, value=option_value: self.handle_option_click(dropdown, text, value),
                 )
             dropdown.add_widget(option_button)
+        dropdown.select(defaultOption)
+
+    def handle_option_click(self, dropdown, option, value):
+        print("option click: option: {} value: {}".format(option, value))
+        settings[self.key] = value 
+        dropdown.select(option)
+    
+    def handle_dropdown_click(self, button, text):
+        print("dropdown text: {}".format(text))
+        setattr(button, 'text', text)
 
 class AgaseedApp(App):
     def build(self):
-        root = GridLayout(cols=2)
-        layout = BoxLayout(orientation='vertical', spacing=10)
-        layout.add_widget(Dropdowns("Glitches", glitches_options))
-        layout.add_widget(Dropdowns("Item Placement", item_placement_options))
-        layout.add_widget(Dropdowns("Glitches", glitches_options))
-        layout.add_widget(Dropdowns("Glitches", glitches_options))
-        layout.add_widget(Dropdowns("Glitches", glitches_options))
-        layout.add_widget(Dropdowns("Glitches", glitches_options))
-        layout.add_widget(Dropdowns("Glitches", glitches_options))
-        layout.add_widget(Dropdowns("Glitches", glitches_options))
-        layout.add_widget(Dropdowns("Glitches", glitches_options))
-        root.add_widget(layout)
-        root.add_widget(Button(text="right"))
+        Window.clearcolor = [1, 1, 1, 1]
+        Window.size = (1334, 750)
+        root = GridLayout(cols=2, size=(Window.width, Window.height))
+        layout = BoxLayout(orientation='vertical', spacing=10, size_hint_y=None, size=(Window.width, Window.height))
+        layout.add_widget(Label(text="Item Placement", halign="left", size_hint_y=None, height=50, color=(0, 0, 0, 1)))
+        layout.add_widget(Dropdowns("glitches", "Glitches", glitches_options, "None"))
+        layout.add_widget(Dropdowns("item_placement", "Item Placement", item_placement_options, "Advanced"))
+        layout.add_widget(Dropdowns("shuffle", "Shuffle", shuffle_options, "Standard"))
+        layout.add_widget(Dropdowns("accessibility", "Accessibility", accessibility_options, "Inventory"))
+        layout.add_widget(Label(text="Goal", halign="left", size_hint_y=None, height=50, color=(0, 0, 0, 1)))
+        layout.add_widget(Dropdowns("goal", "Goal", goal_options, "Ganon"))
+        layout.add_widget(Dropdowns("open_tower", "Open Tower", open_tower_options, "7"))
+        layout.add_widget(Dropdowns("ganon_vulnerable", "Ganon Vulnerable", ganon_vulnerable_options, "7"))
+        layout.add_widget(Label(text="Gameplay", halign="left", size_hint_y=None, height=50, color=(0, 0, 0, 1)))
+        layout.add_widget(Dropdowns("world_state", "World State", world_state_options, "Open"))
+        layout.add_widget(Dropdowns("entrances", "Entrance Shuffle", entrance_shuffle_options, "None"))
+        layout.add_widget(Dropdowns("boss_shuffle", "Boss Shuffle", boss_shuffle_options, "None"))
+        layout.add_widget(Dropdowns("enemy_shuffle", "Enemy Shuffle", enemy_shuffle_options, "None"))
+        layout.add_widget(Dropdowns("hints", "Hints", hints_options, "On"))
+        layout.add_widget(Label(text="Difficulty", halign="left", size_hint_y=None, height=50, color=(0, 0, 0, 1)))
+        layout.add_widget(Dropdowns("swords", "Swords", swords_options, "Randomized"))
+        layout.add_widget(Dropdowns("item_pool", "Item Pool", item_pool_options, "Normal"))
+        layout.add_widget(Dropdowns("item_function", "Item Function", item_function_options, "Normal"))
+        layout.add_widget(Dropdowns("enemy_damage", "Enemy Damage", enemy_damage_options, "Default"))
+        layout.add_widget(Dropdowns("enemy_health", "Enemy Health", enemy_health_options, "Default"))
+        right_layout = BoxLayout(orientation='vertical', spacing=10)
+        right_layout.add_widget(GenerateSeedButton(text="Generate Seed", size_hint_y=None, size_hint_x=0.3))
+        right_layout.add_widget(Label(text="", halign="left", size_hint_y=None, height=50, color=(0, 0, 0, 1)))
+        scroll = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+        scroll.add_widget(layout)
+        root.add_widget(scroll)
+        root.add_widget(right_layout)
         return root
 
 
 if __name__ == "__main__":
-    AgaseedApp().run()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        AgaseedApp().async_run()
+    )
